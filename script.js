e4ace8e1c428b81416df0a0b95026e9f31a4b4f9
Connor <90352499+Connor-vb@users.noreply.github.com>
2021-12-23 11:10:31 -0800

    started login system

--- script.js
+++ script.js
@@ -2,7 +2,29 @@
 
 // for online::::::: <span class="oi oi-circle-check"></span>
 
-$('.dropdown-toggle').dropdown()
+
+// Dropdown toggle for profile
+$('.dropdown-toggle').dropdown();
+
+
+// Log in start
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
+var expressValidator = require('express-validator');
+var flash = require('connect-flash');
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local'),Strategy;
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+var mongoose.connect('mongodb://localhost/projectTwo');
+var db = mongoose.connection;
+
+
+
 
 
 
